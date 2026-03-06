312be3b805537a94ce3049cf4b20388aefcafe92
daboo35 <daboogoog@gmail.com>
2026-03-06 21:18:24 +0300

    Добавлена инициализация VK Bridge и настройка сборки Vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,33 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import vkBridge from '@vkontakte/vk-bridge';
 import './index.css';
 import { App } from './App';
 
-// Рендерим приложение немедленно
-// VK Bridge инициализируется внутри компонентов при необходимости
+// Компонент для инициализации VK Bridge
+function VKBridgeInit({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Инициализируем VK Bridge только если запущены внутри VK
+    if (vkBridge.isWebView()) {
+      console.log('[VK Bridge] Запуск внутри VK, инициализация...');
+      vkBridge.send('VKWebAppInit')
+        .then(() => console.log('[VK Bridge] Инициализация успешна'))
+        .catch((err) => console.warn('[VK Bridge] Ошибка инициализации:', err));
+    } else {
+      console.log('[VK Bridge] Запуск вне VK, инициализация пропущена');
+    }
+  }, []);
+
+  return <>{children}</>;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <VKBridgeInit>
+        <App />
+      </VKBridgeInit>
     </HashRouter>
   </StrictMode>
 );
